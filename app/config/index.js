94c79c41ad7ccb5296816879aa8e88c038688ac5
Mrinmoy Das <mrinmoyfun@gmail.com>
2018-08-11 15:45:20 +0530

    Update index.js

--- app/config/index.js
+++ app/config/index.js
@@ -2,16 +2,16 @@
 
 var init = function () {
 
-	if(process.env.NODE_ENV === 'production') {
+	
 		var redisURI 		= require('url').parse(process.env.REDIS_URL);
 		var redisPassword 	= redisURI.auth.split(':')[1];
 		return {
 			db: {
-				username: process.env.dbUsername,
-				password: process.env.dbPassword,
-				host: process.env.dbHost,
-				port: process.env.dbPort,
-				name: process.env.dbName
+				username: "medgag",
+				password: "madhurima@123",
+				host: "mds155461.mlab.com",
+				port: 55461,
+				name: "medgag"
 			},
 			sessionSecret: process.env.sessionSecret,
 			facebook: {
@@ -33,9 +33,7 @@ var init = function () {
 			}
 		}
 	}
-	else {
-		return require('./config.json');
-	}
+	
 }
 
 module.exports = init();
